f0f040ab846a1995278139ce248106d0f824a7d4
HMB-Entertainment <holdmybeer@protonmail.com>
2017-04-28 19:00:52 -0700

    Update MidiGen.hs
    fixed!

--- code/music/MidiGen.hs
+++ code/music/MidiGen.hs
@@ -7,12 +7,12 @@ right = (line $ map ($en) [a 4, b 4, cs 5, d 5]) :+: e 5 hn
 wrong = (line $ map ($en) [a 4, af 4, g 4, gf 4]) :+: f 4 hn
 delta = line $ map ($en) [a 4, b 4, c 5, b 4, a 4, af 4, gf 4, af 4]
 
-empty = a 3 en 
-x = g 3 en 
-y = f 3 en 
+empty = a 2 en 
+x = b 2 en 
+y = c 3 en 
 xy = e 3 en 
 z = d 3 en 
-zx = c 3 en 
-zy = b 2 en 
-xyz = a 2 en
+zx = f 3 en 
+zy = g 3 en 
+xyz = a 3 en
 
